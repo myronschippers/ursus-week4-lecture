a2c24fcfdd98e9c80d1ab105f8c2bcf80cf37ff8
Myron <dubtwee@gmail.com>
2020-02-20 18:33:23 -0600

    adding lecture content to codebase

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,40 @@
 console.log('Hello Ursus');
+
+function sayHey() {
+  // WHERE WE MAKE MAGIC HAPPEN
+  console.log('HELLO!!!');
+
+  return true;
+}
+
+console.log(sayHey());
+
+const basketOfFruit = [];
+const maxFruit = 8;
+
+function putFruitInBasket(fruitName) {
+  if (maxFruit > howMuchFruit()) {
+    basketOfFruit.push(fruitName);
+  }
+}
+
+function howMuchFruit() {
+  console.log('WOOT');
+  return basketOfFruit.length;
+}
+
+putFruitInBasket('Apple');
+putFruitInBasket('Pear');
+putFruitInBasket('Kiwi');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+putFruitInBasket('Orange');
+console.log('basketOfFruit:', basketOfFruit);
+const fruitCount = howMuchFruit();
+console.log(fruitCount);
